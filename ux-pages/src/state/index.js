33f1afad65d7296a1b3f1a530dca3d503c7b4859
Oyejide <victormoyin435@gmail.com>
2023-12-07 02:21:09 +0100

    FrontEnd has began

---
+++ ux-pages/src/state/index.js
@@ -0,0 +1,37 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+    mode: "light",
+    user: "Oyejide Damilola",
+    token: null,
+    posts: []
+};
+
+
+export const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        setMode: (state) => {
+            state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+        },
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        },
+        setPost: (state, action) => {
+            state.posts.push(action.payload.post);
+        },
+
+    }
+});
+
+export const { setMode, setLogin, setLogout, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
