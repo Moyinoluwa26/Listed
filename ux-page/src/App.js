8b3ba74234c54f2c1ff193cb208e92b596768e44
Oyejide <victormoyin435@gmail.com>
2023-12-07 02:59:14 +0100

    FrontEnd still

--- ux-page/src/App.js
+++ ux-page/src/App.js
@@ -2,15 +2,10 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function App() {
-  ;
   return (
     <div className="App">
-
       <h1>
         {useSelector(state => state.auth.user)}
-      </h1>
-      <h1>
-        Text here :
       </h1>
     </div>
   );
--- ux-page/src/index.js
+++ ux-page/src/index.js
@@ -12,6 +12,7 @@ const store = configureStore({
   }
 });
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
