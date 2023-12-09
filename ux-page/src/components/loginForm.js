10455c63d7965f39e81acfb677c27cdbf217acf4
Oyejide <victormoyin435@gmail.com>
2023-12-09 22:59:47 +0100

    5th day

--- ux-page/src/components/loginForm.js
+++ ux-page/src/components/loginForm.js
@@ -1,7 +1,50 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 const Login = () => {
-    return (<div>Login Page</div>);
+    const Dispatch = useDispatch();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const username = e.target.username.value;
+        const password = e.target.password.value;
+
+        try {
+            const response = await fetch('http://localhost:4000/api/auth/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({ username, password })
+            });
+            const data = await response.json();
+            console.log(data);
+            /*if (data.status === 'ok') {
+                localStorage.setItem('token', data.token);
+                localStorage.setItem('username', data.username);
+                history.push('/');
+            }*/
+
+            if (data.status === 'ok') {
+                Dispatch({ type: 'setLogin', payload: { user: data.user, token: data.token } });
+            }
+        } catch (err) {
+            console.log(err.message);
+        }
+    }
+
+
+    return (<div>
+
+
+
+
+        <form className='flex flex-col mt-2' onSubmit={handleSubmit}>
+            <input type="text" name="username" placeholder="Username" className='p-2 mx-2 border-2 my-2 ' />
+            <input type="password" name="password" placeholder="Password" className='p-2 mx-2 border-2 my-2 ' />
+            <button className='bg-blue-600 text-white p-2 rounded-lg my-2 mx-2 ' type='submit'>Login</button>
+        </form>
+    </div>);
 }
 
 export default Login;
--- ux-page/src/components/registerForm.js
+++ ux-page/src/components/registerForm.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const registerForm = () => {
-    return (<div>Register Form </div>);
+    return (<div>
+        <form className='flex flex-col mt-2' >
+            <input type="text" name="firstname" placeholder="FirstName" className='p-2 mx-2 border-2 my-2 ' />
+            <input type="text" name="lastname" placeholder="LastName" className='p-2 mx-2 border-2 my-2 ' />
+            <input type="number" name="age" placeholder="Your Age" className='p-2 mx-2 border-2 my-2 ' />
+            <input type="text" name="email" placeholder="email" className='p-2 mx-2 border-2 my-2 ' />
+            <input type="text" name="username" placeholder="Choose a suitable Username for yourself" className='p-2 mx-2 border-2 my-2 ' />
+            <input type="text" name="password" placeholder="Choose a suitable password for yourself" className='p-2 mx-2 border-2 my-2 ' />
+            <button className='bg-blue-600 text-white p-2 rounded-lg my-2 mx-2 ' type='submit'>Register</button>
+        </form>
+    </div>);
 }
 
 export default registerForm;
